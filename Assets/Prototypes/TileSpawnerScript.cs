30f9943a74fd568eeee4744c8c7573f322963805
Robert <packleader@dumbdogentertainment.com>
2018-05-29 23:14:12 -0500

    -- updating prototype mechanic scene with tile spawners and updating tile prefabs;

---
+++ Assets/Prototypes/TileSpawnerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TileSpawnerScript : MonoBehaviour
+{
+	public float minSpawnCooldown = 0.15f;
+
+	public float maxSpawnCooldown = 0.25f;
+	
+	private float spawnCooldownRemaining;
+
+	[SerializeField]
+	private GameObject[] TilePrefabs;
+
+	private Collider2D spawnerCollider;
+	
+	void Start()
+	{
+		this.spawnerCollider = GetComponent<Collider2D>();
+		this.spawnCooldownRemaining = Random.Range(this.minSpawnCooldown, this.maxSpawnCooldown);
+	}
+
+	void Update()
+	{
+		this.spawnCooldownRemaining -= Time.deltaTime;
+
+		if(spawnCooldownRemaining <= 0f)
+		{
+			SpawnTile();
+			this.spawnCooldownRemaining = Random.Range(this.minSpawnCooldown, this.maxSpawnCooldown);
+		}
+	}
+
+	private void SpawnTile()
+	{
+		if (this.spawnerCollider.IsTouchingLayers())
+		{
+			return;
+		}
+		
+		var randomIndex = Random.Range(0, this.TilePrefabs.Length);
+		Instantiate(this.TilePrefabs[randomIndex], this.transform.position, Quaternion.identity);
+	}
+}
